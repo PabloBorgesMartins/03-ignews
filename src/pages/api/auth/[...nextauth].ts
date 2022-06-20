08801e9988e3ac50a7a5de93a1a9211cf6031215
Pablo Borges Martins <pablo.martins1998@gmail.com>
2022-06-20 02:31:56 -0300

    update: faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,41 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
-    // Configure one or more authentication providers
-    providers: [
-      GithubProvider({
-        clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        // scope: 'read-user'
-      }),
-      // ...add more providers here
-    ],
-  })
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      // authorization: {
+      //   params: {
+      //     scope: "read_user",
+      //   },
+      // },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email)),
+              ),
+            ),
+            q.Create(q.Collection('users'), { data: { email: user.email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email))),
+          ),
+        );
+        return true
+      } catch (error) {
+        console.log(error)
+        return false;
+      }
+    },
+  },
+  // secret: process.env.NEXTAUTH_URL,
+})
