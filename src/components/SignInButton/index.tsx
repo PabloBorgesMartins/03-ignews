b2305e5692ee5eb4a4f95ed200eb17b1936d1264
Pablo Borges Martins <pablo.martins1998@gmail.com>
2022-06-19 22:19:20 -0300

    feat: Login with Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,29 @@
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from "next-auth/react"
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data: session, status } = useSession();
+    const loading = status === "loading";
 
-    return isUserLoggedIn ? (
+    console.log("Session", session)
+
+    return session ? (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361" />
-            Pablo Borges
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign In with Github
