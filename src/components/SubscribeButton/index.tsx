acdb3397da123d8096d9ccf0d7feafb5054afe5f
Pablo Borges Martins <pablo.martins1998@gmail.com>
2022-07-06 23:23:25 -0300

    Update: ApiStripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn } from "next-auth/react"
+import { api } from "../../services/api";
 import styles from './styles.module.scss';
 
 interface SubscripeButtonProps {
@@ -9,19 +10,26 @@ export function SubscribeButton({ priceId }: SubscripeButtonProps) {
 
     const { data: session, status } = useSession();
 
-    function handleSubcribe() {
-        if(!session){
+    async function handleSubscribe() {
+        if (!session) {
             signIn('github');
             return;
         }
 
-        
+        try {
+            const response = api.post('/subscribe');
+
+            const { sessionId } = response.data;
+        } catch (error) {
+
+        }
     }
 
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
