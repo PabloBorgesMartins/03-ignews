ee4a38131f7a7fb15aa18bf525f25168f52d02e2
Pablo Borges Martins <pablo.martins1998@gmail.com>
2022-07-09 18:00:25 -0300

    feat: Save Subscriptions in Fauna

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Readable } from 'stream';
 import Stripe from 'stripe';
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from './_lib/manageSubscription';
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -41,6 +42,23 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
         const { type } = event;
 
         if (relevantEvents.has(type)) {
+            try {
+                switch (type) {
+                    case 'checkout.session.completed':
+
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session;
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString()
+                        );
+
+                        break;
+                    default:
+                        throw new Error("Unhandled event");
+                }
+            } catch (error) {
+                return response.json({ error: "Webhook handler failed." })
+            }
 
         }
 
