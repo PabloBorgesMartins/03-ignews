8db919d56b334bcd7285f90e71a5c19662328b33
Pablo Borges Martins <pablo.martins1998@gmail.com>
2022-07-20 03:07:51 -0300

    feat: Validate Person's Signature

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn } from "next-auth/react"
+import { useRouter } from "next/router";
 import { api } from "../../services/api";
 import { getStripeJs } from "../../services/stripe-js";
 import styles from './styles.module.scss';
@@ -10,10 +11,16 @@ interface SubscripeButtonProps {
 export function SubscribeButton({ priceId }: SubscripeButtonProps) {
 
     const { data: session, status } = useSession();
+    const router = useRouter();
 
     async function handleSubscribe() {
         if (!session) {
             signIn('github');
+            return;
+        }
+
+        if (session.activeSubscription) {
+            router.push("/posts");
             return;
         }
 
